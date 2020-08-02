00cdce8c8c738d285e6e8ffbe6033b215b065dcd
justinpoblete <42792105+justinpoblete@users.noreply.github.com>
2020-08-02 01:32:30 -0700

    Add files via upload

---
+++ comments.js
@@ -0,0 +1,46 @@
+(function (window) {
+  'use strict';
+
+  const BUTTON_SELECTOR = '[data-posts="id"]';
+
+  let buttons = document.querySelectorAll(BUTTON_SELECTOR);
+
+  buttons.forEach(function (button) {
+    'use strict';
+
+    let sectionSelector = `#comments-${button.value}`;
+    let commentSection = document.querySelector(sectionSelector);
+
+    button.addEventListener('click', function (event) {
+      if (commentSection.hidden) {
+        commentSection.hidden = false;
+        button.textContent = 'Hide comments';
+      } else {
+        commentSection.hidden = true;
+        button.textContent = 'Show comments';
+      }
+
+      event.preventDefault();
+    });
+  });
+})(window);
+
+fetch('http://jsonplaceholder.typicode.com/posts')
+  .then(function(response){
+    return response.json()
+  })
+  .then((response) => {
+    console.log(response);
+    var result = document.getElementById('app');
+    const regex = /\n/g;
+    response.forEach(post => {
+      var temp = post.body;
+      var temp = temp.replace(regex, '<br>');
+      result.innerHTML += `<article>
+      <h2>${post.title}</h2>
+      <p>${temp}</p>
+      </article>`;
+    });
+
+
+  })
